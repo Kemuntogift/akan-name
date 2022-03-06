08c23528f89908d0b84ea6a8e4f2d50a98e0c0b3
Kemunto Gift <giftoyugi@gmail.com>
2022-03-06 21:40:11 +0300

    add event listener and event object

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const myForm = document.getElementById("myForm");
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+myForm.addEventListener("submit", (e) =>
