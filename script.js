069619e431f8d854a5de69a76cb0c9d6bedf6d43
Kemunto Gift <giftoyugi@gmail.com>
2022-03-06 20:52:39 +0300

    add array of female names to select from

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const myForm = document.getElementById("myForm");
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
